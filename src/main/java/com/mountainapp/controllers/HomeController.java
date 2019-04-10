99f95b0cd3fbdd9648411edc80514acb20157c0c
Surfboy678 <Surfboy678@users.noreply.github.com>
2019-04-10 18:02:37 +0200

    added templates & requests

--- src/main/java/com/mountainapp/controllers/HomeController.java
+++ src/main/java/com/mountainapp/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package com.mountainapp.controllers;
 
+import com.mountainapp.models.dtos.MountainDto;
 import com.mountainapp.services.MountainService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -28,6 +31,11 @@ public class HomeController {
     @GetMapping("/delete")
     public String deleteMountain(@RequestParam(value = "mountain") String mountainName){
         mountainService.deleteMountain(mountainName);
-        return "redirects:/mountain";
+        return "redirect:/mountains";
+    }
+    @PostMapping("/addMountain")
+    public String addMountain(@ModelAttribute MountainDto mountain){
+        mountainService.addMountain(mountain);
+        return "redirect:/mountains";
     }
 }
